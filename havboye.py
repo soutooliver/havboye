7150608d219d833f2cf1037bb7d0abd9348b94ab
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 10:31:20 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -5,23 +5,6 @@ import numpy as np
 
 st.title('Havbøye prosjekt')
 
-df = pd.read_csv("dir/Innhenttest.csv")
-
-@st.cache
-def convert_df(df):
-   return df.to_csv().encode('utf-8')
-
-
-csv = convert_df(df)
-
-st.download_button(
-   "Press to Download",
-   csv,
-   "file.csv",
-   "text/csv",
-   key='download-csv'
-)
-
 def start():
     file=open("Innhenttest.csv","a")
     file.flush()
