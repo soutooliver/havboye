2744d65d11f505a5c1ac096cf53ac940146c1304
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 10:19:39 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -1,8 +1,18 @@
+import csv
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Havbøye prosjekt')
+
+csvfile = 'Innhenttest.csv'
+delimiter = ','
+
+def open_with_python_csv(Innhenttest):
+    data = []
+    with open(Innhenttest, 'r') as Innhenttest:
+        reader = csv.reader(Innhenttest, delimiter=delimiter)
 
 def start():
     file=open("Innhenttest.csv","a")
