fab61940ddfea470482e4844a816c9fce17040ab
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 10:40:19 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -7,6 +7,10 @@ st.title('Havbøye prosjekt')
 
 st.write("Innhenttest.csv")
 
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python')
+
 def start():
     file=open("Innhenttest.csv","a")
     file.flush()
