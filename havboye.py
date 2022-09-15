f7bcc03cc5b3f8658af73a99270fb4f0914f10ef
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 09:56:24 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -2,7 +2,7 @@
 def start():
     response=send_at_get_resp("AT+CGNSINF")
     print("Ulstein TOF2 Bøye nr1 "+response)
-    file=open("data.csv","a")
+    file=open("innhenttest.csv","a")
     file.write(str(response)+",")
     file.flush()
     file.close()
