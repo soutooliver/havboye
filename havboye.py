e8f00c5e5fc66d2c8ad8f8e0b944ae9c8f0f6e28
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 10:08:45 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Havbøye prosjekt 2022')
+st.title('Havbøye prosjekt')
 
 def start():
     file=open("Innhenttest.csv","a")
