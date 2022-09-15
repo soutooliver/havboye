959e22a73d190d88b54dc1e6c1e97635a88b8df7
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 10:07:53 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -5,8 +5,6 @@ import numpy as np
 st.title('Havbøye prosjekt 2022')
 
 def start():
-    response=send_at_get_resp("AT+CGNSINF")
-    print("Ulstein TOF2 Bøye nr1 "+response)
     file=open("Innhenttest.csv","a")
     file.write(str(response)+",")
     file.flush()
