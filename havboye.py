a2158f21ac38d0c1b8d188a48eb564df1b0e5b9e
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 10:11:24 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -6,8 +6,13 @@ st.title('Havbøye prosjekt')
 
 def start():
     file=open("Innhenttest.csv","a")
-    file.print()
     file.flush()
     file.close()
+    
+    url = 'https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes/blob/master/all/all.csv'
+df = pd.read_csv(url,index_col=0)
+#df = pd.read_csv(url)
+
+print(df.head(5))
     
 start()
