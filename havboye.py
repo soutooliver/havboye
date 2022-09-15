67089a65a2f91158eeb89f21abd46a6fd88c96f9
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-15 10:04:01 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -1,8 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('havboye')
+
 def start():
     response=send_at_get_resp("AT+CGNSINF")
     print("Ulstein TOF2 Bøye nr1 "+response)
     file=open("Innhenttest.csv","a")
-    file.print("Innhenttest.csv")
+    print.file("Innhenttest.csv")
     file.write(str(response)+",")
     file.flush()
     file.close()
