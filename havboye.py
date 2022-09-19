87f7446e26affa7d5982c69f852220b564d651e0
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-19 13:03:53 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -4,7 +4,16 @@ import pandas as pd
 import numpy as np
 
 st.title('Havbøye prosjekt')
-st.write("Innhenttest.csv")
 
-st.file_uploader("Innhenttest.csv")
+from iww.extractor import extractor
+from iww.detector import detector
+from iww.features_extraction.lists_detector import Lists_Detector as LD
+from iww.features_extraction.main_content_detector import MCD
 
+url = "http://sensor.marin.ntnu.no/logs/Gruppe11.txt"
+json_file = "./iconic.json"
+
+extractor.extract(
+    url = url, 
+    destination = json_file
+)
