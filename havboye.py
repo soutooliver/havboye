18cb690ebdaa4c99972a6ab7b80dd16cae2c371a
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-19 13:37:13 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -2,6 +2,13 @@ import csv
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.title('Havbøye prosjekt')
 
+# Create a section for the dataframe statistics
+st.header('Statistics of Dataframe')
+st.write(df.describe())
+# Create a section for the dataframe header
+st.header('Header of Dataframe')
+st.write(df.head())
