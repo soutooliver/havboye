76f9c6c4bb38e45f6fd0c59f632e074c6b8a6d1e
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-09-19 13:43:14 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -4,10 +4,4 @@ import pandas as pd
 import numpy as np
 
 st.title('Havbøye prosjekt')
-
-# Create a section for the dataframe statistics
-st.header('Statistics of Dataframe')
-st.write(df.describe())
-# Create a section for the dataframe header
-st.header('Header of Dataframe')
-st.write(df.head())
+st.write("It's lit. Bussen e parkert")
