b12187db4bccc03a48e993e42fdfefdd7cb49885
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-10-17 14:05:48 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -27,5 +27,7 @@ if upload_file is not None:
    st.write(df.head())
 
    df = pd.DataFrame(
+      {
+         "Første kollone": [1,2,3,4],
     np.random.randn(1, 2) / [50, 50] + [62.3433, 5.8488],
     columns=['lat', 'lon'])
