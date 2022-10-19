2bdb6b13d0e9c23f2b91da17cd23b7446a1277f6
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-10-19 15:52:28 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -1,9 +1,35 @@
+'''
 import csv
 import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+'''
+import streamlit as st
+import numpy as np
+import pandas as pd
 
+
+
+headers = []
+
+
+
+
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein17_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+st.header("Bar_chart_test")
+st.bar_chart(df[['lat','lon']])
+st.dataframe(df)
+
+
+
+st.map(df)
+
+
+
+st.text("Test")
+
+'''
 url = 'http://sensor.marin.ntnu.no/logs/Ulstein17_10.txt'
 request = requests.get(url)
 print(request.text)
