c3bdd0a35f6a8beef35f3ba7d22f6ee6a3cfb473
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-10-24 11:02:25 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -13,34 +13,7 @@ df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['fi
 st.header('Informasjon om bøye 4')
 st.dataframe(df)
 
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=df,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=df,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
+st.pydeck_chart(pydeck_obj=None, use_container_width=False)
 
 st.text("Test")
 
