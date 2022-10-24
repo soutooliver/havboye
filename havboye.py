00adc4b6ccadb3d8fe9b3b2266f72a955f521307
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-10-24 10:26:22 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -6,7 +6,11 @@ import requests
 
 headers = []
 
+st.title('Havbøye prosjekt')
+
 df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+
+st.header('Informasjon om bøye 4')
 st.dataframe(df)
 
 st.map(df)
