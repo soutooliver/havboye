7a55b37a222a91cd6c9aaee13173dfc420fe1b83
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-10-24 10:10:30 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -7,7 +7,7 @@ import requests
 
 headers = []
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein17_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
 st.header("Bar_chart_test")
 st.bar_chart(df[['lat','lon']])
 st.dataframe(df)
@@ -16,7 +16,7 @@ st.map(df)
 
 st.text("Test")
 
-url = 'http://sensor.marin.ntnu.no/logs/Ulstein17_10.txt'
+url = 'http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt'
 request = requests.get(url)
 print(request.text)
 
