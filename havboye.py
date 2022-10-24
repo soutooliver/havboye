e3c120ca0fd7f13833cea83f07e4d10bfc050424
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-10-24 10:07:24 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -9,23 +9,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-
-
 headers = []
-
-
-
 
 df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein17_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
 st.header("Bar_chart_test")
 st.bar_chart(df[['lat','lon']])
 st.dataframe(df)
 
-
-
 st.map(df)
-
-
 
 st.text("Test")
 
