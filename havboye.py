d33ff2085e41afc54c0a12b0af571f3a726e54c9
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-10-24 10:24:11 +0200

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -7,8 +7,6 @@ import requests
 headers = []
 
 df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
-st.header("Bar_chart_test")
-st.bar_chart(df[['lat','lon']])
 st.dataframe(df)
 
 st.map(df)
