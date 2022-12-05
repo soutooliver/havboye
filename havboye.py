556f7c4e623817811fe9aadd5cd8f3f47150abb3
soutooliver <113115727+soutooliver@users.noreply.github.com>
2022-12-05 11:11:36 +0100

    Update havboye.py

--- havboye.py
+++ havboye.py
@@ -19,7 +19,7 @@ df5 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinB5T1.txt", names = ['
 
 st.header('Informasjon om bøye 1')
 st.dataframe(df1)
-
+st.map(df1)
 
 st.header('Informasjon om bøye 2')
 st.dataframe(df2)
